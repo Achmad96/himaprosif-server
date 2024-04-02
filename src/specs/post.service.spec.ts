ddf38369b5268bc42e64e6dcf595a72d81790709
Achmad96 <totsukamail3278@gmail.com>
2024-04-02 09:31:01 +0700

    test: modified post service spec

--- src/specs/post.service.spec.ts
+++ src/specs/post.service.spec.ts
@@ -23,16 +23,48 @@ const posts = [
         authorId: 'A3',
     },
 ];
+
 const firstPost = posts[0];
 const db = {
     post: {
         findMany: jest.fn().mockResolvedValue(posts),
-        findUnique: jest.fn().mockResolvedValue(firstPost),
+        findUnique: jest
+            .fn()
+            .mockImplementation((postSelect) =>
+                posts.find((post) => post.id === postSelect.where.id),
+            ),
         findFirst: jest.fn().mockResolvedValue(firstPost),
-        create: jest.fn().mockReturnValue(firstPost),
-        save: jest.fn(),
-        update: jest.fn().mockResolvedValue(firstPost),
-        delete: jest.fn().mockResolvedValue(firstPost),
+        create: jest
+            .fn()
+            .mockImplementation(
+                (postSelect) => posts.push(postSelect.data) && postSelect.data,
+            ),
+        update: jest.fn().mockImplementation((postSelect) => {
+            const selectedPost = posts.find(
+                (post) =>
+                    post.id === postSelect.where.id &&
+                    post.authorId === postSelect.where.authorId,
+            );
+            selectedPost.title = postSelect.data.title;
+            selectedPost.content = postSelect.data.content;
+            const { title, content, authorId, id } = selectedPost;
+
+            return {
+                title,
+                content,
+                authorId,
+                id,
+            };
+        }),
+        delete: jest.fn().mockImplementation((postSelect) => {
+            const selectedPost = posts.findIndex(
+                (post) =>
+                    post.id === postSelect.where.id &&
+                    post.authorId === postSelect.where.authorId,
+            );
+            // delete posts[selectedPost];
+            return selectedPost;
+        }),
     },
 };
 
@@ -61,8 +93,44 @@ describe('Post Service', () => {
 
     describe('get post by id', () => {
         it('should return post', async () => {
-            const result = await service.getPostById('P1');
-            expect(result).toEqual(firstPost);
+            const result = await service.getPostById('P3');
+            expect(result).toEqual(posts[2]);
+        });
+    });
+
+    describe('create a new post', () => {
+        it('should create a new post', async () => {
+            const result = await service.createPost('A4', 'title4', 'content4');
+            expect(result).toEqual({
+                authorId: 'A4',
+                title: 'title4',
+                content: 'content4',
+                updated_at: new Date(),
+            });
+        });
+    });
+
+    describe('edited post', () => {
+        it('should edited a post', async () => {
+            const result = await service.editPost(
+                'A1',
+                'P1',
+                'title3',
+                'content1',
+            );
+            expect(result).toEqual({
+                authorId: 'A1',
+                id: 'P1',
+                title: 'title3',
+                content: 'content1',
+            });
+        });
+    });
+
+    describe('deleted  post', () => {
+        it('should create a new post', async () => {
+            const result = await service.deletePost('A1', 'P1');
+            expect(result).toBe(0);
         });
     });
 });
